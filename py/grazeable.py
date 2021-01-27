4d2a6541e699586f3767765fef5d78fc6a7ebd1e
Thor Whalen <thorwhalen1@gmail.com>
2021-01-26 16:30:36 -0800

    Update grazeable.py

--- py/grazeable.py
+++ py/grazeable.py
@@ -19,4 +19,29 @@ class GrazeAndPrep:
         tt = t.droplevel([0, 1], axis=1)
         return tt.iloc[list(map(str.isnumeric, tt['№']))]
         
+    @add_url_attr('https://raw.githubusercontent.com/thorwhalen/my_sources/master/github_emojis.json')
+    def emojis_02(self):
+        from functools import cached_property
+        import json
+
+        from py2store import KvReader
+        from graze import graze
+
+        class EmojiUrls(KvReader):
+            """A store of emoji urls. Will automatically download and cache emoji (name, url) map to a local file when first used."""
+            data_source_url = 'https://raw.githubusercontent.com/thorwhalen/my_sources/master/github_emojis.json'
+
+            @cached_property
+            def data(self):
+                b = graze(self.data_source_url)  # does the same thing as Graze()[url]
+                return json.loads(b.decode())
+
+            def __iter__(self):
+                yield from self.data
+
+            def __getitem__(self, k):
+                return self.data[k]
+            
+        return EmojiUrls()
+        
         
